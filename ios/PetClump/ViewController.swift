ada806c11d6118d1b1dbd56a38f0a837c0bc88c4
zero41120 <tlin34@ucsc.edu>
2018-04-13 15:19:36 -0700

    Adds the function call to setup the view and resizes the label size

--- ios/PetClump/ViewController.swift
+++ ios/PetClump/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
 
     override func didReceiveMemoryWarning() {
