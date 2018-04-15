fe031a1b3b3cd5963822ecd1d4f0916568f1d4cb
Shuoen1997 <sli112@ucsc.edu>
2018-04-15 14:32:42 -0700

    Adds pet ImageButton

--- android/app/src/main/java/com/petclump/petclump/UserInfoActivity.java
+++ android/app/src/main/java/com/petclump/petclump/UserInfoActivity.java
@@ -1,20 +1,43 @@
 package com.petclump.petclump;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class UserInfoActivity extends AppCompatActivity {
-    ImageView user_profile_view, pet_profile_view;
-
-
+    ImageView user_profile;
+    Button button_add_pets;
+    ImageButton pets_profile_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-        user_profile_view = findViewById(R.id.user_profile);
-        pet_profile_view = findViewById(R.id.pet_profile);
+
+        Context c = getApplicationContext();
+
+        user_profile = findViewById(R.id.user_profile);
+
+        pets_profile_view = findViewById(R.id.user_pet1);
+        pets_profile_view.setOnClickListener(v -> {
+            //Starting a new Intent
+            Intent nextScreen = new Intent(c, PetInfoActivity.class);
+            startActivity(nextScreen);
+        });
+
+        button_add_pets = findViewById(R.id.button_add_pet);
+        button_add_pets.setOnClickListener(v -> add_pets());
+    }
+    private void add_pets(){
 
     }
 
