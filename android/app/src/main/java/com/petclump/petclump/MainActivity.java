012bf08d98d35e0938f23c0c22254748ba7d5d35
zero41120 <tlin34@ucsc.edu>
2018-04-14 20:31:39 -0700

    Adds a specie enum with common pets

--- android/app/src/main/java/com/petclump/petclump/MainActivity.java
+++ android/app/src/main/java/com/petclump/petclump/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.petclump.petclump.models.Specie;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
             Intent nextScreen = new Intent(getApplicationContext(), UploadPhotoActivity.class);
             startActivity(nextScreen);
         });
+        ((TextView)findViewById(R.id.hello)).setText(Specie.DOG.getName(getApplicationContext()));
     }
 }
