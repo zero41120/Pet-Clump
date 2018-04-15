64141a606ad2106f7f37e20160811e845f2662d0
Shuoen1997 <sli112@ucsc.edu>
2018-04-14 20:31:50 -0700

    Sets up UserInfoActivity

--- android/app/src/main/java/com/petclump/petclump/MainActivity.java
+++ android/app/src/main/java/com/petclump/petclump/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button pickButton;
+    Button pickButton, settingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         pickButton.setOnClickListener(v -> {
             //Starting a new Intent
             Intent nextScreen = new Intent(getApplicationContext(), UploadPhotoActivity.class);
+            startActivity(nextScreen);
+        });
+
+        settingsButton = findViewById(R.id.button_settings);
+        settingsButton.setOnClickListener(v -> {
+            //Starting a new Intent
+            Intent nextScreen = new Intent(getApplicationContext(), UserInfoActivity.class);
             startActivity(nextScreen);
         });
     }
