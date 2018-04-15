3986317c1b736c53add540dcaa565e1a50800875
zero41120 <tlin34@ucsc.edu>
2018-04-14 21:55:49 -0700

    Adds a simple loop to check specie translated value

--- android/app/src/main/java/com/petclump/petclump/MainActivity.java
+++ android/app/src/main/java/com/petclump/petclump/MainActivity.java
@@ -1,39 +1,60 @@
 package com.petclump.petclump;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.petclump.petclump.models.Specie;
 
 public class MainActivity extends AppCompatActivity {
 
     Button pickButton, settingsButton;
+    TextView hello;
+    Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        c = getApplicationContext();
+
         pickButton = findViewById(R.id.button_go_upload_photo);
         pickButton.setOnClickListener(v -> {
             //Starting a new Intent
-            Intent nextScreen = new Intent(getApplicationContext(), UploadPhotoActivity.class);
+            Intent nextScreen = new Intent(c, UploadPhotoActivity.class);
             startActivity(nextScreen);
         });
 
         settingsButton = findViewById(R.id.button_settings);
         settingsButton.setOnClickListener(v -> {
             //Starting a new Intent
-            Intent nextScreen = new Intent(getApplicationContext(), UserInfoActivity.class);
+            Intent nextScreen = new Intent(c, UserInfoActivity.class);
             startActivity(nextScreen);
         });
 
-        ((TextView)findViewById(R.id.hello)).setText(Specie.DOG.getName(getApplicationContext()));
+        hello = findViewById(R.id.hello);
+
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        for (Specie s: Specie.values()) {
+                            Thread.sleep(1000);
+                            runOnUiThread( ()-> hello.setText(s.getName(c)));
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Log.d("Interrupted", "run: " + e.getMessage());
+                }
+            }
+        };
+        t.start();
 
     }
 }
