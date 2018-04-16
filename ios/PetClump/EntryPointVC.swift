55625ba690d029d402be19417e4d47b16ccb102b
zero41120 <tlin34@ucsc.edu>
2018-04-16 15:57:42 -0700

    Formats sign out action

--- ios/PetClump/EntryPointVC.swift
+++ ios/PetClump/EntryPointVC.swift
@@ -18,20 +18,7 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate {
     @IBOutlet weak var signInButton: GIDSignInButton!
     @IBOutlet weak var signOutButton: UIButton!
     
-    @IBAction func topSignOut(_ sender: Any) {
-        do {
-            let firebaseAuth = Auth.auth()
-            var message = "You have already signed out"
-            if let user = firebaseAuth.currentUser{
-                message = "You have signed out as " + user.displayName
-            }
-            try firebaseAuth.signOut()
-            self.makeAlert(message: message)
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
-            self.makeAlert(message: "Signed out error!")
-        }
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance().uiDelegate = self
@@ -41,19 +28,40 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    /**
+     * This action signs out the user if signed in, then alerts the result.
+     * - Parameter sender: the action sender
+     */
+    @IBAction func topSignOut(_ sender: Any) {
+        // Gets the authentication instance
+        let firebaseAuth = Auth.auth()
+        // Sets up message
+        var message = "You have already signed out"
+        // Signs out the user
+        if let user = firebaseAuth.currentUser{
+            do {
+                try firebaseAuth.signOut()
+                message = "You have signed out as " + user.displayName
+            } catch let signOutError as NSError {
+                message = String.init(format: "Error signing out: %@", signOutError)
+            }
+        }
+        // Alterts result
+        self.makeAlert(message: message)
+    }
     
+    /**
+     * This method make a alter with OK button.
+     * - Parameter message: A string mess to show in the alter
+     */
     func makeAlert(message: String){
         // Make alert
         let alert = UIAlertController(title: "Message", message: message, preferredStyle: UIAlertControllerStyle.alert)
-        
         // add an action (button)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-        
         // show the alert
         self.present(alert, animated: true, completion: nil)
-        
     }
 }
