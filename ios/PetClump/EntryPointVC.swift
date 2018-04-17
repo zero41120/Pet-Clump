00613d508155b0a88b14f1058b2adaa5a2327ea9
zero41120 <tlin34@ucsc.edu>
2018-04-16 18:34:23 -0700

    Formats code and adds background color change when login/out

--- ios/PetClump/EntryPointVC.swift
+++ ios/PetClump/EntryPointVC.swift
@@ -17,9 +17,11 @@ import FBSDKLoginKit
 
 class EntryPointVC: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDelegate, GIDSignInDelegate{
     
-    @IBOutlet weak var signInButton: GIDSignInButton!
-    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var signInButtonGoogle: GIDSignInButton!
+    @IBOutlet weak var signOutButtonGoogle: UIButton!
     @IBOutlet weak var facebookLoginButtonView: UIView!
+    @IBOutlet weak var uidLabel: UILabel!
+    var signInButtonFacebook: FBSDKLoginButton?
     
     let debugMode = true
     
@@ -30,20 +32,114 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDeleg
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
         GIDSignIn.sharedInstance().uiDelegate = self
-        // GIDSignIn.sharedInstance().signIn() // This auto-signs in
         
         // Creates Facebook sign-in
-        let loginButton = FBSDKLoginButton()
-        loginButton.delegate = self
-        loginButton.center = facebookLoginButtonView.center
-        facebookLoginButtonView.backgroundColor = .clear
-        view.addSubview(loginButton)
+        signInButtonFacebook = FBSDKLoginButton()
+        if let btn = signInButtonFacebook{
+            btn.delegate = self
+            btn.center = facebookLoginButtonView.center
+            facebookLoginButtonView.backgroundColor = .clear
+            view.addSubview(btn)
+        }
+        transitionLoginStateUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    /**
+     * This method sets up UI after login state change
+     */
+    func transitionLoginStateUI(){
+        if let user = Auth.auth().currentUser {
+            view.backgroundColor = .green
+            uidLabel.text = user.uid
+        } else {
+            view.backgroundColor = .yellow
+            uidLabel.text = "You are not logged in"
+        }
     }
     
+    /**
+     * This action signs out any Firebase authenticated user if signed in, then alerts the result.
+     * - Parameter sender: the action sender
+     */
+    @IBAction func topSignOut(_ sender: Any) {
+        // Signs out the user
+        var message = "You have already signed out"
+        if let user = Auth.auth().currentUser{
+            do {
+                try Auth.auth().signOut()
+                message = "You have signed out as " + user.uid
+            } catch let signOutError as NSError {
+                message = String.init(format: "Error signing out: %@", signOutError)
+            }
+        }
+        // Alterts result
+        self.transitionLoginStateUI()
+        self.makeAlert(message: message)
+    }
+    
+    /**
+     * This method checks if Firebase is authenicated by a user account.
+     * - Returns: ture if login
+     */
+    func isSignedIn(alertUid: Bool) -> Bool {
+        // Checks if firebase has alreay authenticated a user
+        if let user = Auth.auth().currentUser{
+            if alertUid {
+                self.makeAlert(message: "You have already signed in as " + user.uid)
+            }
+            return true
+        }
+        return false
+    }
+    
+    func isSignedIn() -> Bool{
+        return isSignedIn(alertUid: false)
+    }
+    
+    /**
+     * This method authenticates Firebase with the user credential.
+     */
+    func authenticateFirebase(credential: AuthCredential, withError error: Error? ){
+        // Error checking
+        if isSignedIn(alertUid: false){
+            self.transitionLoginStateUI()
+            return
+            
+        }
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        var message = "Sign in failed"
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                message = error.localizedDescription
+            }
+            if let uid = user?.uid{
+                message = "You have logged in as " + uid
+            }
+            self.transitionLoginStateUI()
+            self.makeAlert(message: message)
+        }
+    }
+    
+    /**
+     * This method make a alter with OK button.
+     * - Parameter message: A string mess to show in the alter
+     */
+    func makeAlert(message: String){
+        // Make alert
+        let alert = UIAlertController(title: "Message", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    // ===============
+    //     Google
+    // ===============
     @available(iOS 9.0, *)
     func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
         -> Bool {
@@ -56,25 +152,12 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDeleg
      * This method implements the protocal when the user clicks Google logout button.
      */
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-        if isSignedIn(alertUid: debugMode){ return }
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
         
         // Authenticates Firebase with the Google user
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
                                                        accessToken: authentication.accessToken)
-        Auth.auth().signIn(with: credential) { (user, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            if let uid = user?.uid{
-                self.makeAlert(message: "You have logged in with Google as " + uid);
-            }
-        }
+        authenticateFirebase(credential: credential, withError: error)
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
@@ -90,22 +173,10 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDeleg
      * This method implements the protocal when the user clicks Facebook loggin button.
      */
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if isSignedIn(alertUid: debugMode){ return }
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
         // Authenticates Firebase with the Facebook user
+        guard (FBSDKAccessToken.current() != nil) else { return }
         let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-        Auth.auth().signIn(with: credential) { (user, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                return
-            }
-            if let uid = user?.uid {
-                self.makeAlert(message: "You have logged in with Facebook as " + uid);
-            }
-        }
+        authenticateFirebase(credential: credential, withError: error)
     }
     
     /**
@@ -116,61 +187,7 @@ class EntryPointVC: UIViewController, GIDSignInUIDelegate, FBSDKLoginButtonDeleg
         if let uid = Auth.auth().currentUser?.uid{
             message = "You have logged out Facebook as " + uid
         }
-        self.makeAlert(message: message);
-    }
-    
-    // ===============
-    //    Common
-    // ===============
-    
-    /**
-     * This action signs out any Firebase authenticated user if signed in, then alerts the result.
-     * - Parameter sender: the action sender
-     */
-    @IBAction func topSignOut(_ sender: Any) {
-        // Gets the authentication instance
-        let firebaseAuth = Auth.auth()
-        // Sets up message
-        var message = "You have already signed out"
-        // Signs out the user
-        if let user = firebaseAuth.currentUser{
-            do {
-                try firebaseAuth.signOut()
-                message = "You have signed out as " + user.uid
-            } catch let signOutError as NSError {
-                message = String.init(format: "Error signing out: %@", signOutError)
-            }
-        }
-        // Alterts result
-        self.makeAlert(message: message)
-    }
-    
-    /**
-     * This method checks if Firebase is authenicated by a user account.
-     * - Returns: ture if login
-     */
-
-    func isSignedIn(alertUid: Bool) -> Bool {
-        // Checks if firebase has alreay authenticated a user
-        if let user = Auth.auth().currentUser{
-            if alertUid {
-                self.makeAlert(message: "You have already signed in as " + user.uid)
-            }
-            return true
-        }
-        return false
-    }
-    
-    /**
-     * This method make a alter with OK button.
-     * - Parameter message: A string mess to show in the alter
-     */
-    func makeAlert(message: String){
-        // Make alert
-        let alert = UIAlertController(title: "Message", message: message, preferredStyle: UIAlertControllerStyle.alert)
-        // add an action (button)
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-        // show the alert
-        self.present(alert, animated: true, completion: nil)
+        makeAlert(message: message);
+        transitionLoginStateUI()
     }
 }
