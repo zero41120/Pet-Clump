94a1d42b2a902652889a972bc18b3b6200751c0b
zero41120 <tlin34@ucsc.edu>
2018-04-22 11:44:08 -0700

    Adds a basic upload page with 2 inputs

---
+++ ios/PetClump/FirestoreLearningVC.swift
@@ -0,0 +1,28 @@
+//
+//  FirestoreLearningVC.swift
+//  PetClump
+//
+//  Created by YSH on 4/22/18.
+//  Copyright © 2018 PetClump. All rights reserved.
+//
+
+
+import UIKit
+import Firebase
+
+class FirestoreLearningVC: UIViewController{
+    
+    @IBOutlet weak var topInput: UITextField!
+    @IBOutlet weak var butInput: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var fetchButton: UIButton!
+    
+    let debugMode = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
