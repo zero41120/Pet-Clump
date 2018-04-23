5a5f3a8ca8d86026f0c0c380459ef4ec66a1672b
zero41120 <tlin34@ucsc.edu>
2018-04-22 21:56:07 -0700

    Adds download button and label to check communiction

--- ios/PetClump/Model/Profile.swift
+++ ios/PetClump/Model/Profile.swift
@@ -11,54 +11,83 @@ import CoreLocation
 import Firebase
 
 protocol Profile {
-    var id: String      { get set }
-    var name: String    { get set }
-    var birthday: Date  { get set }
-    var gender: String  { get set }
     func generateDictionary() -> [String: Any]
     func upload(vc: QuickAlert)
 }
 
 
+/**
+ * This is the onwer profile data model
+ * - id: this should be the uid from Firebase Auth object
+ * - name: this should be user's name to show to public
+ * - birthday: this is a Date object. In the initlizer, it's a yyyy/MM/dd string
+ * - gender: a string of any gender
+ * - distancePerference: this shows the user's perference for matching
+ * - lat and lon: for location calculation
+ * - freeTime: this is a 7*3 bool matrix for free time. In the initlizer, it's a 21 character string with 1 mark as free.
+ */
 class OwnerProfile: Profile{
-   
     
-    var id: String
-    var name: String
-    var birthday: Date
-    var gender: String
+    let id: String
+    var name: String = "No naoe"
+    var birthday: Date = Date()
+    var gender: String = "Apache"
     var distancePerference: Int = 5
-    var freeTime: FreeSchedule = FreeSchedule(freeString: "")
+    var lat: Double = 0.0
+    var lon: Double = 0.0
+    var freeTime = FreeSchedule(freeString: "")
     
-    init(id: String, name: String, birthday: Date, gender: String) {
-        self.id = id
-        self.name = name
-        self.birthday = birthday
-        self.gender = gender
+    /**
+     * This is the id initializer. When loading a profile, only logged in user
+     * may construct this object.
+     */
+    init(id: String) {
+        if let _ = Auth.auth().currentUser?.uid{
+            self.id = id
+        } else {
+            self.id = "error_id"
+        }
+    }
+    
+    convenience init(dic: [String: Any]){
+        print("Constructing with: " + dic.description)
+        self.init(id: dic["id"] as! String)
+        self.name = dic["name"] as? String ?? self.name
+        self.lat = dic["lat"] as? Double  ?? self.lat
+        self.lon = dic["lon"] as? Double ?? self.lon
+        self.gender = dic["gender"] as? String ?? self.gender
+        self.distancePerference = dic["distancePerference"] as? Int ?? self.distancePerference
+        self.freeTime = FreeSchedule(freeString: dic["freeTime"] as? String ?? "")
     }
     
     func generateDictionary() -> [String : Any] {
         return [
-            "id": self.id,
-            "name": self.name,
-            "birthday": self.birthday,
-            "gender": self.gender,
-            "distancePerference": self.distancePerference,
-            "freeTime": self.freeTime.freeString
+            "id":   id ,
+            "lat":  lat ,
+            "lon":  lon ,
+            "name": name ,
+            "gender":   gender ,
+            "birthday": birthday ,
+            "freeTime": freeTime.freeString,
+            "distancePerference": distancePerference
         ]
     }
+    
     func upload(vc: QuickAlert) {
         guard Auth.auth().currentUser != nil else {
             vc.makeAlert(message: "User is not signed in!")
             return
         }
-        let docRef = Firestore.firestore().document("users/owner_profile")
+        
+        let docRef = Firestore.firestore().collection("users").document(id)
         docRef.setData(self.generateDictionary()) { (err: Error?) in
             if let err = err{
                 vc.makeAlert(message: "Upload failed, reason:" + err.localizedDescription)
             }
+            print("Uploaded successfully for user " + self.id)
         }
     }
+
 }
 
 class FreeSchedule{
