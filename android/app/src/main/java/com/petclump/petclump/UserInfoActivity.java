5a694a58badc338c3ad93c672dced457c7086e62
zero41120 <tlin34@ucsc.edu>
2018-04-24 22:30:33 -0700

    Downloads user infromation with firebase for android's user setting view

--- android/app/src/main/java/com/petclump/petclump/UserInfoActivity.java
+++ android/app/src/main/java/com/petclump/petclump/UserInfoActivity.java
@@ -6,37 +6,53 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
-public class UserInfoActivity extends AppCompatActivity {
-    private int number_of_pets = 1;
-    ImageButton button_add_pet, button_why;
-    CircularImageView profile_pet1, profile_pet2, profile_pet3;
-    TextView name_pet1, name_pet2, name_pet3;
-    TextView user_name, user_gender, user_dob, user_match_range;
-    Button edit_button;
-    Context c;
-    ConstraintLayout constraintLayout;
-    ConstraintSet constraintSet;
+import java.util.Date;
+import java.util.Map;
 
+public class UserInfoActivity extends AppCompatActivity {
+    private static final String TAG = "User Info Activity";
+    private int number_of_pets = 1;
+    private ImageButton button_add_pet, button_why;
+    private CircularImageView profile_pet1, profile_pet2, profile_pet3;
+    private TextView name_pet1, name_pet2, name_pet3;
+    private TextView name_label, gender_label, birthday_label, range_label;
+    private Button edit_button;
+    private Context c;
+    private ConstraintLayout constraintLayout;
+    private ConstraintSet constraintSet;
+    private Date birthday;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        if(user == null){
+            Log.d(TAG, "onCreate: User not logged in");
+            finish();
+        }
+        setupUI();
+        downloadData();
+    }
+
+    private void setupUI(){
         setContentView(R.layout.activity_user_info);
-
-
         c = getApplicationContext();
-
-        constraintSet = new ConstraintSet();
+        constraintSet    = new ConstraintSet();
         constraintLayout = findViewById(R.id.user_info_layout);
         constraintSet.clone(constraintLayout);
 
@@ -44,49 +60,60 @@ public class UserInfoActivity extends AppCompatActivity {
         profile_pet2 = findViewById(R.id.profile_pet2);
         profile_pet3 = findViewById(R.id.profile_pet3);
 
+        name_label      = findViewById(R.id.name_label);
+        gender_label    = findViewById(R.id.gender_label);
+        birthday_label  = findViewById(R.id.birthday_label);
+        range_label     = findViewById(R.id.match_range_seekbar);
 
-        user_name = findViewById(R.id.user_name);
-        user_gender = findViewById(R.id.user_gender);
-        user_dob = findViewById(R.id.user_dob);
-        user_match_range = findViewById(R.id.user_match_range);
-        edit_button = findViewById(R.id.edit_button);
+        edit_button     = findViewById(R.id.save_button);
 
-
-        if (2 == number_of_pets) {
-            profile_pet2.setVisibility(View.VISIBLE);
-            //name_pet2.setVisibility(View.VISIBLE);
-        } else if (3 == number_of_pets) {
-            profile_pet3.setVisibility(View.VISIBLE);
-            profile_pet3.setVisibility(View.VISIBLE);
-            //name_pet2.setVisibility(View.VISIBLE);
-            //name_pet3.setVisibility(View.VISIBLE);
-        }
-        profile_pet1.setOnClickListener(v -> {
-            //Starting a new Intent
-            Intent nextScreen = new Intent(c, PetInfoActivity.class);
-            startActivity(nextScreen);
-        });
+        profile_pet1.setOnClickListener(v ->
+            startActivity(new Intent(c, PetInfoActivity.class))
+        );
 
         button_add_pet = findViewById(R.id.button_add_pet);
         button_add_pet.setOnClickListener(v -> add_pets());
 
         button_why = findViewById(R.id.button_why);
-        button_why.setOnClickListener(v -> {
-            startActivity(new Intent(c, Popup.class));
-        });
+        button_why.setOnClickListener(v ->
+            startActivity(new Intent(c, Popup.class))
+        );
 
-        edit_button.setOnClickListener(v -> {
-            //Starting a new Intent
-            Intent nextScreen = new Intent(c, UserInfoEditActivity.class);
-            startActivity(nextScreen);
+        edit_button.setOnClickListener(v ->
+            startActivity(new Intent(c, UserInfoEditActivity.class))
+        );
+    }
+
+    private void downloadData(){
+        if (user == null){ return; }
+        String uid = user.getUid();
+
+        DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("users").document(uid);
+        mDocRef.addSnapshotListener((snap, error) -> {
+            if (error != null) {
+                Log.d(TAG, "Download failed: " + error.toString());
+                return;
+            }
+            if (snap == null)   { return; }
+            if (!snap.exists()) { return; }
+            Map<String, Object> ref = snap.getData();
+            name_label.setText(ref.get("name").toString());
+            gender_label.setText(ref.get("gender").toString());
+            if (ref.get("birthday") instanceof Timestamp){
+                Timestamp bd = (Timestamp) ref.get("birthday");
+                birthday = bd.getApproximateDate();
+                birthday_label.setText(birthday.toString());
+            }
+            range_label.setText(ref.get("distancePerference").toString());
         });
     }
+
     private void add_pets() {
         if (1 == number_of_pets) {
             number_of_pets += 1;
             profile_pet2.setVisibility(View.VISIBLE);
             //name_pet2.setVisibility(View.VISIBLE);
-//
+
         } else if (2 == number_of_pets) {
             number_of_pets += 1;
             profile_pet3.setVisibility(View.VISIBLE);
@@ -96,9 +123,6 @@ public class UserInfoActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(c, "you've reached the maximum pet number!", Toast.LENGTH_LONG);
             toast.show();
         }
-
     }
-
-
 }
 
