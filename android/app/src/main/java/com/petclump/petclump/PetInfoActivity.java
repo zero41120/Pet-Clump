0e6398870f135363384e66b8fc109df459a945a5
Shuoen1997 <sli112@ucsc.edu>
2018-04-30 16:50:34 -0700

    Adds pet specie spinner
    yeah yeah yeah

--- android/app/src/main/java/com/petclump/petclump/PetInfoActivity.java
+++ android/app/src/main/java/com/petclump/petclump/PetInfoActivity.java
@@ -7,24 +7,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.petclump.petclump.models.OwnerProfile;
 import com.petclump.petclump.models.PetProfile;
+import com.petclump.petclump.models.Specie;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
 public class PetInfoActivity extends AppCompatActivity{
     private static final String TAG = "Pet Info Activity";
-    private TextView pet_name, pet_spe, pet_age, pet_bio, pet_primary_name, pet_and_I_name;
+    private TextView pet_name, pet_age, pet_bio, pet_primary_name, pet_and_I_name;
+    private Spinner pet_specie;
+    private String[] specie_array_string;
     private int sequence = -1;
 
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -54,8 +59,20 @@ public class PetInfoActivity extends AppCompatActivity{
         // set up UI
         pet_age = findViewById(R.id.pet_age);
         pet_bio = findViewById(R.id.pet_bio);
-        pet_spe = findViewById(R.id.pet_specie);
         pet_name = findViewById(R.id.pet_name);
+        pet_specie = findViewById(R.id.pet_specie);
+
+        specie_array_string = new String[19];
+        int i = 0;
+        for (Specie s: Specie.values()){
+            specie_array_string[i] = s.getName(this);
+            i+=1;
+        }
+        ArrayAdapter<String> adapter_specie = new ArrayAdapter<>(this,
+
+                android.R.layout.simple_spinner_item, specie_array_string);
+        adapter_specie.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        pet_specie.setAdapter(adapter_specie);
 
         pet_primary_name = findViewById(R.id.title_pet_name);
         pet_and_I_name = findViewById(R.id.Txt_my_pet_and_I);
@@ -66,7 +83,7 @@ public class PetInfoActivity extends AppCompatActivity{
             pet_and_I_name.setText(profile.getName()+" and I");
             pet_age.setText(profile.getAge());
             pet_bio.setText(profile.getBio());
-            pet_spe.setText(profile.getSpe());
+            pet_specie.setSelection(Specie.num_specie(profile.getSpe().toString()));
             pet_name.setText(profile.getName());
         });
 
@@ -76,18 +93,18 @@ public class PetInfoActivity extends AppCompatActivity{
         Button_return.setOnClickListener(v ->
                 finish()
         );
-        Button_edit.setOnClickListener(v->{
-            Intent i = new Intent(this, PetInfoEditActivity.class);
-            i.putExtra("sequence", sequence);
-            startActivity(i);
-        });
+//        Button_edit.setOnClickListener(v->{
+//            Intent i = new Intent(this, PetInfoEditActivity.class);
+//            i.putExtra("sequence", sequence);
+//            startActivity(i);
+//        });
 
         Button_save.setOnClickListener(v->{
             PetProfile pet = new PetProfile();
             pet.setBio(pet_bio.getText().toString());
             pet.setAge(pet_age.getText().toString());
             pet.setName(pet_name.getText().toString());
-            pet.setSpe(pet_spe.getText().toString());
+            //pet.setSpe(pet_specie.getText().toString());
             pet.setOwner_id(user.getUid());
             pet.setSequence(sequence);
             pet.upload(user.getUid()+sequence,()->{});
