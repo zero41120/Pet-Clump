176f7e2a120ca257adf3461d64804a4d6bce4b6c
jerod zheng <jszheng@ucsc.edu>
2018-05-20 18:58:43 -0700

    uplates the friendlist view

--- ios/PetClump/controller/FrinedListViewVC.swift
+++ ios/PetClump/controller/FrinedListViewVC.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class FrinedListViewVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //this is the array for each fof your riend's name
     let elements = ["horse", "cat", "dog", "potato","horse", "cat", "dog", "potato","horse", "cat", "dog", "potato"]
+    
+    // this is the array for each of the friend's chat history
+    let message = ["Hello", "Hi", "How are you", "Nice to meet you","Hello", "Hi", "How are you", "Nice to meet you","Hello", "Hi", "How are you", "Nice to meet you" ]
+    
+    // this is the array for each of the friend's time last message sent.
+    let time = ["12:40", "15:35", "08:55", "23:11", "12:40", "15:35", "08:55", "23:11", "12:40", "15:35", "08:55", "23:11"]
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -31,12 +38,22 @@ class FrinedListViewVC: UIViewController, UITableViewDelegate, UITableViewDataSo
     
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //load each cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! FriendListTableViewCell
         
+        //loading the name of each frineds
         cell.animalLbl.text = elements[indexPath.row]
-        cell.animalChat.text = elements[indexPath.row]
-        cell.animalTime.text = elements[indexPath.row]
+        
+        //loading the image of each friend
         cell.animalImage.image = UIImage(named: elements[indexPath.row])
+        //make the image round
+        cell.animalImage.layer.cornerRadius = cell.animalImage.frame.height / 2
+        
+        //load the message of each friend
+        cell.animalChat.text = message[indexPath.row]
+        //load the time of each friend chat
+        cell.animalTime.text = time[indexPath.row]
         
         
         return cell
