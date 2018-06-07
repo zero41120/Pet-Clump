356a13ea314e94ea4f764e74609d5eb973b46310
shuoen1997 <sli112@ucsc.edu>
2018-06-06 22:17:14 -0700

    Adds sp;ash screen

---
+++ android/app/src/main/java/com/petclump/petclump/controller/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.petclump.petclump.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.petclump.petclump.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
